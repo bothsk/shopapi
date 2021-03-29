e683d0a0529f904e5646822b470d71b842b0e029
bothsk <both-sittikorn@hotmail.com>
2021-03-29 13:50:38 +0700

    reset password with out email

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,24 +1,30 @@
 const User = require('../models/user')
 const Order = require('../models/order')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const validator = require('email-validator')
 
 const user_regis = async (req,res) =>{
-    const username = req.body.username
-    const password = req.body.password
+    const {username,password,email,isAdmin} = req.body
     try {
-        if (username&&password){
+        if (username&&password&&email){
+            if (!userValid(username,password,email)) return res.json({status:{error:true,message:message}})
             const existedUser = await User.findOne({username:username})
             if (existedUser) return res.json({status:{error:true,message:'Username already existed'}})
-    
-            const hash = bcrypt.hash(password,10)
-            const detail  = {
+
+            const existedEmail = await User.findOne({email:email})
+            if (existedEmail) return res.json({status:{error:true,message:'Email already existed'}})
+
+            const hash = bcrypt.hashSync(password,10)
+            const createdUser = await User.create({
                 username:username,
-                password:hash
-            }
-            const createdUser = await User.create(detail)
+                password:hash,
+                email:email,
+                isAdmin:isAdmin
+            })
             return res.json({status:{error:null,message:'Account Created'},createdUser})
            
-        }  return res.json({status:{error:true,message:'Please input username and password'}})
+        }  return res.json({status:{error:true,message:'Please input username, password and email'}})
     } catch (err) {
         
         return res.json({status:{error:true,message:'DB processing error'},err:err.message})
@@ -27,6 +33,26 @@ const user_regis = async (req,res) =>{
     
 }
 
+
+let message 
+const userValid = (username,password,email)=>{
+    if (username.length<6) {
+        message = "User required at least 6 characters"
+        return false
+    }
+    
+    if (password.length<4){
+        message = "Password required at least 4 characters"
+        return false
+    }
+
+    if (!validator.validate(email)){
+        message = "Wrong email input format"
+        return false
+    }
+
+    return true
+}
 
 
 const user_login =  async (req, res, next)=> {
@@ -57,11 +83,57 @@ const user_order = async (req,res)=>{
 
 }
 
+///////////////////////////
+const user_forget = async (req,res)=>{
+    const { email } = req.body
+    if (!email||!validator.validate(email)) return res.json({status:{error:true,message:'Incorrect email input'}})
+   
+    try {
+        const checkEmail = await User.findOne({email:email})
+    if (!checkEmail) return res.json({status:{error:true,message:'Not found user related to this email'}})
+    
+    const token = jwt.sign({user:checkEmail.username},process.env.jwtSecret+checkEmail.password,({expiresIn:'5m'}))
+    res.send(`http://localhost:3000/reset/${checkEmail.username}/${token}`)
+    } catch (err) {
+        
+    return res.json({status:{error:true,message:'DB processing error'},err:err.message})
+    }
+}
+
+
+const user_reset = async (req,res)=>{
+    const { user,token } = req.params
+    const { password} = req.body
+
+    try{
+        const checkedUser = await User.findOne({username:user})
+        if (!checkedUser) return res.json({status:{error:true,message:`Not found User in DB`}})
+        
+        let checkedToken
+        try {
+            checkedToken = jwt.verify(token,process.env.jwtSecret+checkedUser.password)
+        } catch {
+        return res.json({status:{error:true,message:`Invalid token or token was expired`}})
+        }
+        if (!password)  return res.json({status:{error:true,message:`Please input new password`}})
+        const hashed = bcrypt.hashSync(password,10)
+        const resetUser = await User.findOneAndUpdate({username:checkedToken.user},{password:hashed})
+        if (!resetUser) return res.json({status:{error:true,message:`Not found user related to this token`}})
+
+        return res.json({status:{error:null,message:`User ${resetUser.username} has been reset password`}})  
+    } catch (err) {
+        
+        return res.json({status:{error:true,message:'DB processing error'},err:err.message})
+     }
+}
+
 
 module.exports = {
     user_login,
     user_regis,
     user_order,
     user_failed,
-    user_logout
+    user_logout,
+    user_forget,
+    user_reset
 }
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
-const { user_login,user_regis,user_order,user_failed,user_logout } = require('../controllers/indexController')
+const { user_login,user_regis,user_order,user_failed,user_logout,user_forget,user_reset} = require('../controllers/indexController')
 const { isLoggedIn,isLoggedOut } = require('../passport')
 require('../passport')
 
@@ -18,6 +18,9 @@ router.post('/regis',isLoggedOut,user_regis)
 
 router.post('/login',isLoggedOut,passport.authenticate('local', { failWithError: true }),user_login,user_failed)
 
+router.post('/forget',isLoggedOut,user_forget)
+
+router.post('/reset/:user/:token',isLoggedOut,user_reset)
 
 router.get('/logout',isLoggedIn,user_logout)
 
